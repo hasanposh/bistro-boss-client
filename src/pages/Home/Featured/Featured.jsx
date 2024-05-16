76154d5b0a4c6732417025b8b479962c9a8036f2
hasanposh <hmasif147@gmail.com>
2024-05-17 03:04:58 +0600

    complete with home page

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -1,10 +1,40 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import feturedImage from "../../../assets/home/featured.jpg";
 
 const Featured = () => {
-    return (
-        <div className="flex justc">
-            
+  return (
+    <section className="bg-center bg-cover bg-fixed text-white" style={{
+        backgroundImage: `linear-gradient(45deg,rgba(0,0,0,0.9),rgba(0,0,0,0.3)),url(${feturedImage})`,
+      }}>
+      <div className="flex justc">
+        <SectionTitle subHeading={"Check it out"} heading={"Featured Item"} />
+      </div>
+      <div
+        
+        className={`md:flex justify-center items-center py-20 px-36 `}
+      >
+        <div>
+          <img src={feturedImage} alt="" />
         </div>
-    );
+        <div className="md:ml-10 space-y-4">
+          <p>Aug 20, 2029</p>
+          <p className="uppercase">Where can i get some?</p>
+          <p>
+            Objectively disseminate alternative total linkage after
+            out-of-the-box infomediaries. Professionally seize compelling
+            processes for effective alignments. Compellingly reinvent intuitive
+            data with open-source benefits. Appropriately productize excellent
+            human capital without 2.0 resources. Rapidiously iterate adaptive
+            infrastructures via emerging action items. Dynamically grow top-line
+            information after resource-leveling process improvements.
+          </p>
+          <button className="btn btn-outline border-0 border-b-4 mt-8 border-white text-white">
+            Order Now
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Featured;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,5 +1,7 @@
+import Testimonials from "../../Testimonials/Testimonials";
 import Banner from "../Banner/Banner";
 import Catagory from "../Catagory/Catagory";
+import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
 
 const Home = () => {
@@ -8,6 +10,8 @@ const Home = () => {
       <Banner />
       <Catagory />
       <PopularMenu />
+      <Featured />
+      <Testimonials/>
     </div>
   );
 };
